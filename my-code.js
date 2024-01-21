7a09797c7d25b804087d969f3e444a9dd788560a
Anna-merry <155963798+Anna-merry@users.noreply.github.com>
2024-01-21 14:14:26 +0300

    Update my-code.js

--- my-code.js
+++ my-code.js
@@ -2,4 +2,22 @@ const elements = document.getElementsByClassName("card-img-overlay");
 const screenHeight = window.innerHeight;
 element.style.height = `${screenHeight}px`;
 window.addEventListener("resize", changeFontSize);
+
+
+function validateForm() {
+      const name = document.forms["myForm"]["name"].value;
+      const email = document.forms["myForm"]["email"].value;
+      const message= document.forms["myForm"]["message"].value;
+    
+      if (name == "") {
+        alert("Пожалуйста, введите ваше имя");
+        return false;
+      }
+    
+      if (email == "") {
+        alert("Пожалуйста, введите ваш email");
+        return false;
+      }
+    }
+  
 		
