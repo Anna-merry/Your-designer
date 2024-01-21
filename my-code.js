b8ac7009834acd3f7b066e5c5a5692454482029a
Anna-merry <155963798+Anna-merry@users.noreply.github.com>
2024-01-21 11:35:55 +0300

    Create my-code.js

---
+++ my-code.js
@@ -0,0 +1,5 @@
+const elements = document.getElementsByClassName("card-img-overlay");
+const screenHeight = window.innerHeight;
+element.style.height = `${screenHeight}px`;
+window.addEventListener("resize", changeFontSize);
+		
